4ee44ca616d859b77bef99fa09da6bdc8ed21f88
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-11-10 15:35:16 +0900

    write

--- detected_human/src/detected_human_node.py
+++ detected_human/src/detected_human_node.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
 import roslib
@@ -9,3 +9,22 @@ import numpy as np
 import pandas as pd
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
+
+model = load_model("detected_human_model.h5")
+MAX_RANGE = 6
+DATASIZE  = 725
+
+x_desire = 0
+y_desire = 0
+
+def callback(data):
+  l_data = pd.DataFrame(data.ranges).fillna(0)
+  l_data = np.reshape(l_data["l"].values/MAX_RANGE)
+  result = model.predict(l_data)
+  print(result)
+
+def detect_torso():
+  rospy.init_node("detected_human")
+  rospy.Subscriber("/scan", LaserScan, callback)
+  print("ok")
+  rospy.spin()
