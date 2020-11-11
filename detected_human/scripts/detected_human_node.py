fbfe0c8b5077f10a65608922292d449237c144c2
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-11-11 12:52:58 +0900

    class化

--- detected_human/scripts/detected_human_node.py
+++ detected_human/scripts/detected_human_node.py
@@ -10,27 +10,26 @@ from keras.models import load_model
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
 
-rospy.init_node("detected_human")
-pub = rospy.Publisher("/human_point", Float32MultiArray, queue_size=10)
+class deepLearnModel():
+  def __init__(self):
+    self.sub = rospy.Subscriber("/scan", LaserScan, callback)
+    self.pub = rospy.Publisher("/human_point", Float32MultiArray, queue_size=10)
+    self.MAX_RANGE = 6
+    self.DATASIZE  = 726
+    self.model = load_model("/home/daidai/catkin_ws/src/detected_human/scripts/detected_human_model.h5")
 
-model = load_model("/home/daidai/catkin_ws/src/detected_human/scripts/detected_human_model.h5")
+  def callback(self, data):
+    l_np = np.nun_to_num(data.ranges)
+    l_np = np.resize(l_np, (1, self.DATASIZE, 1))
+    pre = model.predict(l_np/self.MAX_RANGE)
+    pre = np.reshape(pre, -1)
+    x_desire = pre[0]*np.cos(pre[1])
+    y_desire = pre[0]*np.sin(pre[1])
 
-def callback(data):
-  MAX_RANGE = 6
-  DATASIZE  = 726
-  l_np = np.nun_to_num(data.ranges)
-  l_np = np.resize(l_np, (1, DATASIZE, 1))
-  pre = model.predict(l_np/MAX_RANGE)
-  pre = np.reshape(pre, -1)
-  x_desire = pre[0]*np.cos(pre[1])
-  y_desire = pre[0]*np.sin(pre[1])
-  arr = Float32MultiArray([x_desire, y_desire])
-  print(arr)
-
-def detect_torso():
-  rospy.Subscriber("/scan", LaserScan, callback)
-  print("ok")
-  rospy.spin()
+    msg = Float32MultiArray()
+    msg.data = np.asarray([x_desire, y_desire])
+    self.pub.publish(msg)
 
 if __name__ == '__main__':
-  detect_torso()
+  rospy.init_node("detected_human")
+  rospy.spin()
