20a28fc3ce3492681d32c514b18427285f2fedf4
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-11-11 13:21:27 +0900

    動作確認済

--- detected_human/scripts/detected_human_node.py
+++ detected_human/scripts/detected_human_node.py
@@ -12,16 +12,17 @@ from std_msgs.msg import Float32MultiArray
 
 class deepLearnModel():
   def __init__(self):
-    self.sub = rospy.Subscriber("/scan", LaserScan, callback)
+    self.sub = rospy.Subscriber("/scan", LaserScan, self.callback)
     self.pub = rospy.Publisher("/human_point", Float32MultiArray, queue_size=10)
     self.MAX_RANGE = 6
     self.DATASIZE  = 726
     self.model = load_model("/home/daidai/catkin_ws/src/detected_human/scripts/detected_human_model.h5")
 
   def callback(self, data):
+    print("callbacked")
     l_np = np.nan_to_num(data.ranges)
     l_np = np.resize(l_np, (1, self.DATASIZE, 1))
-    pre = model.predict(l_np/self.MAX_RANGE)
+    pre = self.model.predict(l_np/self.MAX_RANGE)
     pre = np.reshape(pre, -1)
     x_desire = pre[0]*np.cos(pre[1])
     y_desire = pre[0]*np.sin(pre[1])
@@ -32,4 +33,5 @@ class deepLearnModel():
 
 if __name__ == '__main__':
   rospy.init_node("detected_human")
+  node = deepLearnModel()
   rospy.spin()
