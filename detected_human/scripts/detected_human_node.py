ff2210b53f2289a17b5d9867535f966f0cc1a650
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-11-11 12:06:04 +0900

    model 更新

--- detected_human/scripts/detected_human_node.py
+++ detected_human/scripts/detected_human_node.py
@@ -10,22 +10,25 @@ from keras.models import load_model
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
 
-model = load_model("/home/daidai/catkin_ws/src/detected_human/scripts/detected_human_model.h5")
-MAX_RANGE = 6
-DATASIZE  = 725
+rospy.init_node("detected_human")
+rospy.Subscriber("/scan", LaserScan, callback)
+pub = rospy.Publisher("/human_point", Float32MultiArray, queue_size=10)
 
-x_desire = 0
-y_desire = 0
+model = load_model("/home/daidai/catkin_ws/src/detected_human/scripts/detected_human_model.h5")
 
 def callback(data):
-  l_pd = pd.DataFrame(data.ranges).fillna(0)
-  l_np = np.reshape(l_pd.values, (1, DATASIZE, 1))
-  pre = model.predict(l_np)
-  pre = np.reshape(pre, -1)  
+  MAX_RANGE = 6
+  DATASIZE  = 726
+  l_np = np.nun_to_num(data.ranges)
+  l_np = np.resize(l_np, (1, DATASIZE, 1))
+  pre = model.predict(l_np/MAX_RANGE)
+  pre = np.reshape(pre, -1)
+  x_desire = pre[0]*np.cos(pre[1])
+  y_desire = pre[0]*np.sin(pre[1])
+  arr = Float32MultiArray([x_desire, y_desire])
+  print(arr)
 
 def detect_torso():
-  rospy.init_node("detected_human")
-  rospy.Subscriber("/scan", LaserScan, callback)
   print("ok")
   rospy.spin()
 
