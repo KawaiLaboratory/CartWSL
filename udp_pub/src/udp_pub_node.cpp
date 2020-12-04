b12cd5749c8413bcdd1202d54b11e5d81247792d
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-12-04 18:53:00 +0900

    can compile

--- udp_pub/src/udp_pub_node.cpp
+++ udp_pub/src/udp_pub_node.cpp
@@ -12,7 +12,7 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
   double l = 0.0;     //得られた最近点までの距離[m]
   double theta = 0.0; //得られた最近点の角度[rad]
   ros::Time now = ros::Time::now();
-  std::ofstream fs(std::to_string(now.toSec())+".csv");  // CSVファイル生成
+  std::ofstream fs("~/catkin_ws/src/udp_pub/csvs/"+std::to_string(now.toSec())+".csv");  // CSVファイル生成
   fs << "l, theta" << std::endl;
 
   for(int i = 0; i < 726; i++) {
@@ -34,13 +34,16 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "udp_pub");
   ros::NodeHandle n;
   ros::Subscriber sub;
+  ros::Publisher pub;
   ros::Rate sleep_rate(20);
 
   sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
   pub = n.advertise<std_msgs::Float32MultiArray>("/u_cbf", 1000);
 
   std_msgs::Float32MultiArray msg;
-  msg.data = [0, 0.1];
+  msg.data.resize(2);
+  msg.data[0] = 0;
+  msg.data[1] = 0.1;
 
   pub.publish(msg);
   while(cnt < 100){
