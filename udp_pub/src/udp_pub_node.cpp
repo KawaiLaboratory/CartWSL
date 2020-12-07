9bafdf9ba2134d33a8d58f551692d7a0eb7609e0
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-12-07 14:22:44 +0900

    csvs

--- udp_pub/src/udp_pub_node.cpp
+++ udp_pub/src/udp_pub_node.cpp
@@ -12,18 +12,14 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
   double l = 0.0;     //得られた最近点までの距離[m]
   double theta = 0.0; //得られた最近点の角度[rad]
   ros::Time now = ros::Time::now();
-  std::ofstream fs("~/catkin_ws/src/udp_pub/csvs/"+std::to_string(now.toSec())+".csv");  // CSVファイル生成
-  fs << "l, theta" << std::endl;
+  std::ofstream fs(std::to_string(now.toSec())+".csv");  // CSVファイル生成
+  fs << "l, theta,range_max" << std::endl;
 
   for(int i = 0; i < 726; i++) {
     l = scan->ranges[i];
     theta = scan->angle_min + scan->angle_increment * i;
 
-    if(theta > scan->angle_max){
-      break;
-    }else{
-      fs << l << "," << theta << std::endl;
-    }
+    fs << l << "," << theta << "," << scan->range_max << std::endl;
   }
   fs.close();
   cnt += 1;
@@ -38,6 +34,8 @@ int main(int argc, char **argv)
   ros::Rate rate(20);
   ros::Rate sleep(0.1);
 
+  sleep.sleep();
+
   sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
   pub = n.advertise<std_msgs::Float32MultiArray>("/u_cbf", 1000);
 
@@ -46,12 +44,20 @@ int main(int argc, char **argv)
   msg.data[0] = 0;
   msg.data[1] = 0.1;
 
-  sleep.sleep();
+  ros::Time start = ros::Time::now();
+  ros::Time now = ros::Time::now();
 
-  while(cnt < 100){
+  while((now-start).toSec() < 80){
     pub.publish(msg);
+
     rate.sleep();
     ros::spinOnce();
+
+    now = ros::Time::now();
   }
- return 0;
+
+  msg.data[1] = 0;
+  pub.publish(msg);
+  ROS_INFO("csv size is %d", cnt);
+  return 0;
 }
